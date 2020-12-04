4ccf7b4af10954cce2aa40aac04023539347220e
Marie-Joseph <none@ya.biz>
2020-12-03 21:17:16 -0600

    Setup AppImage generation

--- install.sh
+++ install.sh
@@ -1,18 +1,60 @@
 #!/bin/sh
 
+export VERSION="0.1";
 PROG_NAME=bladjad
-INSTALL_DIR=AppDir
-FONTS_DIR=fonts
-IMAGES_DIR=images
+STD_PREFIX=usr
+APPIMAGE_PREFIX=AppDir
+PREFIX=$APPIMAGE_PREFIX/$STD_PREFIX
+BIN_DIR=$PREFIX/bin
+LIB_DIR=$PREFIX/lib
+DATA_DIR=$PREFIX/share/$PROG_NAME
+DESKTOP_DIR=$PREFIX/share/applications
+FONTS_DIR=$DATA_DIR/fonts
+IMAGES_DIR=$DATA_DIR/images
+ICONS_DIR=$PREFIX/share/icons/hicolor
+DUB_DIR=.dub
+DUB_PKG_DIR=$DUB_DIR/packages
 
 if [ ! -e $PROG_NAME ]; then
-    dub build;
+    echo "--------------------";
+    echo "Building $PROG_NAME...";
+    dub build -q;
 fi;
 
-if [ ! -d $INSTALL_DIR ]; then
-    mkdir $INSTALL_DIR;
+if [ ! -d $PREFIX ]; then
+    linuxdeploy --appdir $APPIMAGE_PREFIX;
 fi;
 
-cp -r $IMAGES_DIR $INSTALL_DIR/$IMAGES_DIR;
-cp -r $FONTS_DIR $INSTALL_DIR/$FONTS_DIR;
-mv $PROG_NAME $INSTALL_DIR/$PROG_NAME;
+if [ ! -d $DATA_DIR ]; then
+    mkdir -p $DATA_DIR;
+fi;
+
+if [ ! -d $DESKTOP_DIR ]; then
+    mkdir -p $DESKTOP_DIR;
+fi;
+
+echo "--------------------";
+echo "Copying resources...";
+cp -r images $IMAGES_DIR;
+cp -r fonts $FONTS_DIR;
+
+
+echo "--------------------";
+echo "Copying desktop files..."l
+cp $PROG_NAME.desktop $DESKTOP_DIR/
+cp icons/16x16/bladjad-icon.png $ICONS_DIR/16x16/apps/
+cp icons/32x32/bladjad-icon.png $ICONS_DIR/32x32/apps/
+cp icons/64x64/bladjad-icon.png $ICONS_DIR/64x64/apps/
+cp icons/128x128/bladjad-icon.png $ICONS_DIR/128x128/apps/
+cp icons/256x256/bladjad-icon.png $ICONS_DIR/256x256/apps/
+
+echo "--------------------";
+echo "Installing binary...";
+mv $PROG_NAME $BIN_DIR/$PROG_NAME;
+
+echo "--------------------";
+echo "Second linuxdeploy...";
+linuxdeploy --appdir $APPIMAGE_PREFIX --output appimage;
+
+echo "--------------------";
+echo "Done~!";
